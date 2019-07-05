abfd4adc251a14c4b91f90dd4ba57356c76f12e1
Sunny Au <sunny.au@outlook.com>
2019-07-05 22:26:09 +0100

    integration-test profile

---
+++ src/test/java/com/company/aws/IntegrationTestIT.java
@@ -0,0 +1,13 @@
+package com.company.aws;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class IntegrationTestIT {
+    @Test
+    public void integrationTestShouldRun()
+    {
+        assertTrue( true );
+    }
+}
